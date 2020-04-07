ebbf63318df80f987e47ef0120844ff31066757e
ahammadshawki8 <ahammadshawki8@gmail.com>
2020-04-07 20:05:35 +0600

    latest update

---
+++ # module_of_pandas.py
@@ -0,0 +1,5 @@
+# we are going to use jupyter notebook for this series.
+# open jupyter notebook in command prompt using this command.
+"""jupyter notebook"""
+# or go to this link
+"C:\Users\User\AhammadShawki8\Jupyter Notebook"
